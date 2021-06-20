dcd576633dcb0faa1022b459904f5ed61e742fbc
Sona Elizebeth Shaji <56604740+Sonaelizebeth@users.noreply.github.com>
2021-06-20 12:31:23 +0530

    Add files via upload

---
+++ Largest.py
@@ -0,0 +1,10 @@
+#program to check the largest number among 3 numbers
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+c = int(input("Enter the third number:"))
+if(a>c):
+    print("A is Largest")
+elif (b>c):
+    print("B is largest")
+else:
+    print("C is largest")
