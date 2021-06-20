bcc7614976513d1c2d4cdc755fd608bf3cabe973
Sona Elizebeth Shaji <56604740+Sonaelizebeth@users.noreply.github.com>
2021-06-20 12:31:05 +0530

    Add files via upload

---
+++ Voting.py
@@ -0,0 +1,7 @@
+#Checking the eligibilty to vote by age
+age = int(input("Enter your age:"))
+if(age>=18):
+    print("You are eligible to cast your vote!")
+else:
+    print("Ineligible to vote!!!")
+print("Thank You!")
