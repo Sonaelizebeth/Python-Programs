765b584395314ad362f275ea1f27866900bc8880
Sona Elizebeth Shaji <56604740+Sonaelizebeth@users.noreply.github.com>
2021-06-22 12:45:32 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,13 @@
+num=int(input("Enter number:"))
+if(num>2):
+    for i in range(2, num):
+        if(num%i==0):
+            print(bool())
+            break
+        else:
+            print(bool("num"))
+            break
+elif(num==2):
+    print(bool("num"))
+else:
+    print(bool())
