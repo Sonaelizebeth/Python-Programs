27348269ba238cad6cc22773069bb5e1c2a980cb
Sona Elizebeth Shaji <56604740+Sonaelizebeth@users.noreply.github.com>
2021-06-24 21:36:43 +0530

    Update aboutMe.py

--- aboutMe.py
+++ aboutMe.py
@@ -1,2 +1,4 @@
 print("My name is Sona Elizebeth Shaji")
-print("")
+print("I'm a Web Developer")
+print(" Pursuing Btech in CS from APJ Abdul Kalam Kerala Technical University")
+print(" Currently studying in St.Josephs College of Engineering and Technology Palai")
